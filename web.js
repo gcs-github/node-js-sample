79257f01083f8373b03305068e96a96be54f7984
Guillaume Ceccarelli <guillaume@gcs-ventures.com>
2013-07-11 20:36:47 +0000

    reading from index.html - Using Buffer class

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(fs.readFileSync('index.html'));
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
